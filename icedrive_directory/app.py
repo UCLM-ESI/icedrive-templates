44eba8e2779133c1eaa9b0b5b08a8f47f22e0309
José Luis Segura Lucas <joseluis.segura@uclm.es>
2023-11-03 23:47:56 +0100

    Initial commit for branch directory

---
+++ icedrive_directory/app.py
@@ -0,0 +1,34 @@
+"""Authentication service application."""
+
+import logging
+import sys
+from typing import List
+
+import Ice
+
+from .directory import Directory
+
+
+class DirectoryApp(Ice.Application):
+    """Implementation of the Ice.Application for the Authentication service."""
+
+    def run(self, args: List[str]) -> int:
+        """Execute the code for the AuthentacionApp class."""
+        adapter = self.communicator().createObjectAdapter("DirectoryAdapter")
+        adapter.activate()
+
+        servant = Directory()
+        servant_proxy = adapter.addWithUUID(servant)
+
+        logging.info("Proxy: %s", servant_proxy)
+
+        self.shutdownOnInterrupt()
+        self.communicator().waitForShutdown()
+
+        return 0
+
+
+def main():
+    """Handle the icedrive-authentication program."""
+    app = DirectoryApp()
+    return app.main(sys.argv)
---
+++ icedrive_directory/directory.py
@@ -0,0 +1,49 @@
+"""Module for servants implementations."""
+
+from typing import List
+
+import Ice
+
+import IceDrive
+
+
+class Directory(IceDrive.Directory):
+    """Implementation of the IceDrive.Directory interface."""
+
+    def getParent(self, current: Ice.Current = None) -> IceDrive.DirectoryPrx:
+        """Return the proxy to the parent directory, if it exists. None in other case."""
+
+    def getChilds(self, current: Ice.Current = None) -> List[str]:
+        """Return a list of names of the directories contained in the directory."""
+
+    def getChild(self, name: str, current: Ice.Current = None) -> IceDrive.DirectoryPrx:
+        """Return the proxy to one specific directory inside the current one."""
+
+    def createChild(
+        self, name: str, current: Ice.Current = None
+    ) -> IceDrive.DirectoryPrx:
+        """Create a new child directory and returns its proxy."""
+
+    def removeChild(self, name: str, current: Ice.Current = None) -> None:
+        """Remove the child directory with the given name if exists."""
+
+    def getFiles(self, current: Ice.Current = None) -> List[str]:
+        """Return a list of the files linked inside the current directory."""
+
+    def getBlobId(self, filename: str, current: Ice.Current = None) -> str:
+        """Return the "blob id" for a given file name inside the directory."""
+
+    def linkFile(
+        self, filename: str, blob_id: str, current: Ice.Current = None
+    ) -> None:
+        """Link a file to a given blob_id."""
+
+    def unlinkFile(self, filename: str, current: Ice.Current = None) -> None:
+        """Unlink (remove) a filename from the current directory."""
+
+
+class DirectoryService(IceDrive.DirectoryService):
+    """Implementation of the IceDrive.Directory interface."""
+
+    def getRoot(self, user: str, current: Ice.Current = None) -> IceDrive.DirectoryPrx:
+        """Return the proxy for the root directory of the given user."""
